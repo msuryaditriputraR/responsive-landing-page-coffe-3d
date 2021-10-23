8fb37bacaa8e3bacf9a3b2599ea7dc7781e9d87f
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2021-10-23 12:25:22 +0800

    add gsap animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,51 @@ function moveImage(e) {
     layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
   });
 }
+
+// === GSAP ANIMATION ===
+
+// NAV
+gsap.from(".nav__logo, .nav__toggle", {
+  opacity: 0,
+  duration: 1,
+  delay: 2,
+  y: 10,
+});
+
+gsap.from(".nav__item", {
+  opacity: 0,
+  duration: 1,
+  delay: 2.1,
+  y: 30,
+  stragger: 0.2,
+});
+
+// HOME
+gsap.from(".home__title", {
+  opacity: 0,
+  duration: 1,
+  delay: 1.6,
+  y: 30,
+});
+
+gsap.from(".home__description", {
+  opacity: 0,
+  duration: 1,
+  delay: 2.1,
+  y: 30,
+  stragger: 0.2,
+});
+
+gsap.from(".home__button", {
+  opacity: 0,
+  duration: 1,
+  delay: 2.1,
+  y: 30,
+});
+
+gsap.from(".home__img", {
+  opacity: 0,
+  duration: 1,
+  delay: 1.3,
+  y: 30,
+});
